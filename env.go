b3af07b619b99a5076b66123a0ef98db8d480911
Sam Pool <sambpool@gmail.com>
2022-02-08 15:32:23 -0700

    refactor to allow for env variables in db connection

--- env.go
+++ env.go
@@ -1,9 +1,31 @@
 package main
 
-import (
-	"os"
-)
+import "os"
 
-func SetEnv() {
-	os.Setenv("DB_URL", "host=localhost port=5432 user=sampool dbname=gb_challenge sslmode=disable")
+func SetEnv() (err error) {
+
+	var dbInfo = map[string]string{
+		"DB_NAME": "gb_challenge",
+		"DB_HOST": "localhost",
+		"DB_USER": "postgres",
+		"DB_PASS": "",
+		"DB_PORT": "5432",
+	}
+
+	for key := range dbInfo {
+		currEnv := os.Getenv(key)
+		if currEnv != "" {
+			dbInfo[key] = currEnv
+		}
+	}
+
+	err = os.Setenv(
+		"DB_URL",
+		"host="+dbInfo["DB_HOST"]+
+			" port="+dbInfo["DB_PORT"]+
+			" user="+dbInfo["DB_USER"]+
+			" dbname="+dbInfo["DB_NAME"]+
+			" sslmode=disable",
+	)
+	return
 }
