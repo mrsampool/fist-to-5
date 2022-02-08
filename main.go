8c7009b50793373a2bacb9117f4f5c180f89f532
Sam Pool <sambpool@gmail.com>
2022-02-08 15:34:27 -0700

    extract setupServer for testing

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
+	router, err := setupServer()
+	err = router.Run()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func setupServer() (router *gin.Engine, err error) {
+	router = gin.Default()
 	config := cors.DefaultConfig()
 	config.AllowAllOrigins = true
 	router.Use(cors.New(config))
-	SetEnv()
-	SetRoutes(router)
-	err := router.Run(":8000")
+	err = SetEnv()
 	if err != nil {
 		return
 	}
+	SetRoutes(router)
+	return
 }
